2057486e5e6a671bcdaeb6af15696c1217578c7a
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-11 16:57:05 +0300

    Задача 10

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите положительное целое число");
+int n = Convert.ToInt32 (Console.ReadLine ());
+    if(n > 100)
+    {   while ( n >= 1000)
+    
+            n = n/10;
+       
+        {
+            int x = n%100;
+            int i = x%10;
+        Console.WriteLine( "третья цифра " + i);
+        }
+    }
+    else 
+    {
+        Console.WriteLine( "третья цифра в числе " + n + " отсутствует");
+    }

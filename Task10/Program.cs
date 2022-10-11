ee9c13e65e807a51909528800591033ebdd6f688
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-11 11:05:44 +0300

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число a");
+int a = Convert.ToInt32 (Console.ReadLine ());
+    if (99 < a && a < 1000)
+        {
+            int x = a/10;
+            int i = x%10;
+            Console.WriteLine( "вторая цифра " + i);
+        }
+    else 
+    {
+        Console.WriteLine("Вы ввели некорректное число");
+    }

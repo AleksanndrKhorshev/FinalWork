f2e3c5ea40a20ab63deee0e1adbca2daf9e6d989
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-14 17:07:59 +0300

    Задача21

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координату X первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату X второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+int X = x2 - x1;
+int Y = y2 - y1;
+int Z = z2 - z1;
+Console.WriteLine($"Расстояние между точками {Math.Sqrt (X*X + Y*Y + Z*Z)}");

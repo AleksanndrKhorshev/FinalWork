e1787cbd5750efeee8a5adc85d5b1c01843abf0d
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-14 16:06:57 +0300

    Задача 19

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int N = Convert.ToInt32(Console.ReadLine());
+if ( N > 9999 & N  < 100000 )
+    {
+    int a = N/10000;
+
+    int x = N/1000;
+    int b = x%10;
+
+    int y = N/10;
+    int c = y%10;
+
+    int d = N%10;
+    if (a == d & a == d)
+        {
+            Console.WriteLine("Число палиндром");
+        }
+     
+    else 
+        {
+            Console.WriteLine( "число не палиндром");
+        }
+    }
+else Console.WriteLine("Число не пятизначное");

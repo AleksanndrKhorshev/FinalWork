f05db0428f09ad0928b64b062f95b22eba81ec02
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-16 22:29:18 +0300

    Задача 27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int Summ(int n)
+{
+    int result = 0;
+    while (n > 0)
+    {
+        result += n % 10;
+        n = n / 10;
+    }
+    return result;
+}
+int Sum = Summ(n);
+{
+    Console.Write ($"Сумма цифр числа равна {Sum}");
+}

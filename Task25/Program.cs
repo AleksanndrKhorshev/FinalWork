5153c13e34f9474a46ee0344c67266ab790d10cd
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-16 19:00:20 +0300

    Задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите степень числа");
+int a = Convert.ToInt32 (Console.ReadLine ());
+int i = 2;
+int St = n;
+if (a == 0)
+    {
+        St = 1;
+    }
+while ( i <=a)
+    {
+        St = St*n;
+        i++;
+    }
+
+ Console.WriteLine($"{n} в степени {a} равно {St}");

f8430b0cdb98e4d13db152e2e762d6d9021d46d3
Aleksandr Khorshev <ya.balkonprofi@yandex.ru>
2022-10-16 20:53:01 +0300

    Задача 29

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[]collection)
+ {
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection [index] = new Random().Next(1,1000);
+        index++;
+    }
+
+ }
+
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+Console.WriteLine("Введите длину массива");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+FillArray(array);
+PrintArray(array);
